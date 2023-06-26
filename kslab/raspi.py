491fbfb78577baf2316e6fb1643d0e0f86ebbbe4
Kshora <kurokawa.shora.kyoto@gmail.com>
2023-06-26 18:54:22 +0900

    [fix] adjust to new raspi data format

--- kslab/raspi.py
+++ kslab/raspi.py
@@ -1,155 +1,202 @@
 import pandas as pd
 from graph_tools import *
+from datetime import datetime, date
+import os
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import AutoMinorLocator, LogLocator
+from matplotlib import rcParams
 
 class Raspi:
-    def __init__(self, bpath="", timestamp=""):
-        # from os.path import join, exists, expanduser
-        # from os import makedirs
-        from datetime import date
+
+    def __init__(self, bpath="", timepath=""):
 
         self.bpath = bpath
-        self.stamp = timestamp
+        self.timepath = timepath
         self.date = date.today()
-        # self.ppath = join(expanduser('~'), 'Dropbox','lab','programs','OxygenExperiments','out','raspi',str(self.date))
-        # if not exists(self.ppath):
-        #     makedirs(f'../../out/raspi/{self.date}') #必要に応じてmakedirできるようにしたほうが良い
 
         self.load_data()
-        if self.file_type == "out":
-            self.convert_signals()
-            self.update_signals()
+
+        rcParams["font.size"] = 28
 
     def load_data(self):
         """load data"""
-        from datetime import datetime
-        import os
 
-        if os.path.exists(os.path.join(self.bpath, f"out_{self.stamp}_temp.csv")):
-            self.file_type = "out"
-            self.tc = pd.read_csv(os.path.join(self.bpath, f"out_{self.stamp}_temp.csv"))
-            self.adc = pd.read_csv(os.path.join(self.bpath, f"out_{self.stamp}.csv"))
-        elif os.path.exists(os.path.join(self.bpath, f"cu_{self.stamp}_temp.csv")):
-            self.file_type = "cu"
-            self.tc = pd.read_csv(os.path.join(self.bpath, f"cu_{self.stamp}_temp.csv"),skiprows=7,header=None,names=["date","time","T","PresetT"])
-            self.adc = pd.read_csv(os.path.join(self.bpath, f"cu_{self.stamp}.csv"),skiprows=8,header=None,names=["date","time","P1","P2","Ip","B1","B2","IGmode","IGscale","QMS_signal","P1_c","P2_c","Ip_c","B1_c","B2_c"])
-        else:
-            raise FileNotFoundError(f"out_{self.stamp}_temp.csv or cu_{self.stamp}_temp.csv not found")
+        self.path = [l for l in os.listdir(self.bpath) if l.endswith(".csv") and self.timepath in l]
+        self.data_config = {}
+        skip_rows = 0
+
+        try:
+            self.path = os.path.join(self.bpath,self.path[0])
+            print(self.path)
+            with open(self.path) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row[0].startswith("#"):
+                        if not len(row) == 1:
+                            self.data_config[row[0].replace(' ','').replace('#','').replace(':','')] = row[1:]
+                        skip_rows += 1
+                    else:
+                        break
+
+            self.data = pd.read_csv(self.path, skiprows=skip_rows,names=self.data_config['Columns'])
+        except IndexError:
+            raise FileNotFoundError(f"no data found for {self.timepath}")
         
-        if self.file_type == "out":
-            start = datetime.strptime(self.stamp, "%Y%m%d_%H%M%S")
-            self.adc.insert(0, "date", start + pd.to_timedelta(self.adc["time"], unit="s"))
-            self.tc.insert(0, "date", start + pd.to_timedelta(self.tc["time"], unit="s"))
-        self.start = datetime.strptime(self.stamp, "%Y%m%d_%H%M%S")
-        if self.file_type == "out":
-            self.end = self.tc['date'][len(self.tc)-1].to_pydatetime()
-        else:
-            pass
+    def update_data(self):
+        self.data['datetime'] = self.data['date'].apply(lambda x: int(x.translate(str.maketrans({' ': '', '-': '', ':': ''})).split('.')[0]))
+        self.start_time = self.data['datetime'].iloc[0]
+        self.end_time = self.data['datetime'].iloc[-1]
 
-    def convert_signals(self):
-        """convert ADC signals from raw volts to proper units
-        pu: upstream pressure, plasma chamber
-        pd: downstream pressure, permeation chamber
-        ip: plasma current
+    def prepera_dataset(self):
+        self.experiment_dataset = []
+        self.calibration_dataset = []
+        data = pd.DataFrame()
+        flag = 0
+        for i in range(len(self.data)):
+            col = self.data.iloc[[i]]
+            if col['QMS_signal'].iloc[0] == 1:
+                data = pd.concat([data, col])
+                flag = 1
+            elif col['QMS_signal'].iloc[0] == 2:
+                data = pd.concat([data, col])
+                flag = 2
+            if col['QMS_signal'].iloc[0] == 0 and flag == 1:
+                if len(data) > 20:
+                    self.experiment_dataset.append(data)
+                    flag = 0
+                    data = pd.DataFrame()
+                else:
+                    flag = 0
+                    data = pd.DataFrame()
+            elif col['QMS_signal'].iloc[0] == 0 and flag == 2:
+                if len(data) > 20:
+                    self.calibration_dataset.append(data)
+                    flag = 0
+                    data = pd.DataFrame()
+                else:
+                    flag = 0
+                    data = pd.DataFrame()
 
-        """
-        # 11.46 convert Pfeiffer signal to pressure
-        self.pu = 10 ** (1.667 * self.adc["P2"] - 11.46)
-        # convert linear IG signal to pressure
-        self.pd = self.adc["P1"] * 10 ** self.adc["IGscale"]
-        # plasma current from hall sensor
-        def iconv(v):
-            return 5 / 1 * (v - 2.52)
-
-        self.ip = iconv(self.adc["Ip"])
-        self.t = self.adc["date"]
-
-    def update_signals(self):
-        """Update corresponding columns in self.adc with
-        converted ones
-        """
-        self.adc.insert(1, "pu", self.pu)
-        self.adc.insert(1, "pd", self.pd)
-        self.adc.insert(1, "ip", self.ip)
-
+        
     def plot(self, **kws):
-        """Plot time traces"""
-        import matplotlib.dates as mdates
-        import matplotlib.pylab as plt
-        from matplotlib.ticker import LogLocator, AutoMinorLocator
+        """plot data"""
+        self.plot_data = kws.get("data", None)
+        if self.plot_data is None:
+            self.time_formatter(**kws)
+        
 
-        fro = kws.get("fro", self.start)
-        tto = kws.get("tto", self.end)
+        
 
-        a = fro.strftime("%Y%m%d%H%M%S")
-        b = tto.strftime("%Y%m%d%H%M%S")
-        df_adc = self.adc.query(f"{a} < date < {b}")
-        df_tc = self.tc.query(f"{a} < date < {b}")
+        fig = plt.figure(figsize=(16, 9),dpi=50,facecolor='w')
 
-        fig = plt.figure(figsize=(12, 8), facecolor="w")
-        gs = plt.GridSpec(4, 1)
-        gs.update(left=0.1, right=0.95, wspace=0.2, hspace=0.14)
-        ax0 = plt.subplot(gs[:2, 0])  # sharex=ax0
-        axs = [ax0] + [plt.subplot(gs[i, 0], sharex=ax0) for i in range(2, 4)]
+    def plot_pressure(self,**kws):
+        # plot pressure data
+        """
+        separate plot axis for upstream and downstream
+        ax1 for upstream Pu, Bu
+        ax2 for downstream Pd, Bd
+        """
+        xlabel = kws.get("xlabel", "time")
+        yscale = kws.get("yscale", "linear")
 
-        plottrig = 0
+        st = kws.get("st", self.start_time)
+        et = kws.get("et", self.end_time)
 
-        def plot_trig(axt):
-            axt.plot(df_adc["datetime"], df_adc["QMS_signal"], "C2", label="trig")
-            axt.set_yticks([])
+        self.pu_lim = kws.get("pu_lim", (1e-7, 1e-2))
+        self.pd_lim = kws.get("pd_lim", (1e-8, 1e-5))
 
-        if plottrig:
-            twins = [ax.twinx() for ax in [axs[0], axs[1], axs[2]]]
-            [plot_trig(ax) for ax in twins]
-            twins[0].legend(loc=1, bbox_to_anchor=[0.2, 1.2])
+        plot_upstream = kws.get("plot_upstream", True)
+        plot_downstream = kws.get("plot_downstream", True)
 
-        ax = axs[0]
-        ax.plot(df_adc["date"], df_adc["pd"], "-", c="k", label="Permeation Chamber")
-        ax.plot(df_adc["date"], df_adc["pu"], "-", c="C3", label="Plasma Chamber")
-        ax.locator_params(axis="y", nbins=6)
+        self.plot_data = kws.get("data", None)
+        fig = kws.get("fig", plt.figure(figsize=(16,9),dpi=50,facecolor='w'))
+        ax1 = kws.get("ax1", fig.add_subplot(111)) # plot ax for upstream
+        ax2 = kws.get("ax2", ax1.twinx()) # plot ax for downstream
+        axs = [ax1,ax2]
+        self.time_formatter(st=st,et=et)
+        pressures = {"upstream" :{
+                                "data": ["Pu_c","Bu_c"],
+                                "color": ["#c9004d","#ffb405"], 
+                                "label": ["Pu","Bu"]
+                                },
+                     "downstream" :{
+                                "data": ["Pd_c","Bd_c"],
+                                "color": ["#6ac600","#00a3af"],
+                                "label": ["Pd","Bd"]
+                                }
+                    }
+        
+        if plot_upstream:
+            pressure = pressures["upstream"]
+            [axs[0].plot(self.plot_data[xlabel],self.plot_data[pressure["data"][i]],label=pressure["label"][i],c=pressure["color"][i]) for i in range(2)]
+            axs[0].set_ylim(self.pu_lim)
+            axs[0].set_ylabel('Pressure upstream (Torr)')
+            axs[0].set_xlabel('Time (s)')
+            axs[0].legend(loc='upper left')
+                    
+            
+        if plot_downstream:
+            pressure = pressures["downstream"]
+            [axs[1].plot(self.plot_data[xlabel],self.plot_data[pressure["data"][i]],label=pressure["label"][i],c=pressure["color"][i]) for i in range(2)]
+            axs[1].set_ylim(self.pd_lim)
+            axs[1].set_ylabel('Pressure douwnstream (Torr)')
+            axs[1].legend(loc='upper right')
 
-        ax.legend(loc=1, ncol=2, bbox_to_anchor=[1.0, 1.2])
-
-        ax = axs[1]
-
-        ax.plot(df_adc["date"], df_adc["ip"] - df_adc["ip"][:100].mean(), "k")
-
-        ax = axs[2]
-        ax.plot(df_tc["date"], df_tc["T"], c="k")
-        ax.plot(df_tc["date"], df_tc["PresetT"], "C2--")
-        plt.xticks(rotation=25, ha="right")
-
-        gridalpha = kws.get("gridalpha", [0.1, 0.3])
-        [customgrid(ax, gridalpha=gridalpha) for ax in axs]
-        [customticks(ax) for ax in axs]
-        axs[0].set_yscale("log")
+        [ax.set_yscale(yscale) for ax in axs]
+        if xlabel == "date":
+            xticks_label = [i for n,i in enumerate(self.plot_data[xlabel]) if n%(len(self.plot_data)//10) == 0]
+            ax1.set_xticks(xticks_label)
+            ax1.set_xticklabels(xticks_label,rotation=45)
+            # plt.xticks(rotation=45)
+            # ax1.xaxis.set_minor_locator(AutoMinorLocator(2))
+            # ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d %H:%M"))
+            self.xtick_label = xticks_label
 
 
-        lbls = ["P [Torr]", "$I_p$ [A]", "T [$^{\circ }$C]"]
-        [ax.set_ylabel(l) for l, ax in zip(lbls, axs)]
-        axs[-1].set_xlabel(f"time")
 
-        lims = kws.get("lims", [[5e-8, 2e-2], [-0.2, 2.5], [0, 1000]])
-        [ax.set_ylim(l) for ax, l in zip(axs, lims)]
-        axs[-1].locator_params(axis="y", nbins=5)  # increas number of major ticks for T plot
-        locmax = LogLocator(base=10, subs=(1.0,), numticks=100)
-        axs[0].yaxis.set_major_locator(locmax)
+        axs[1].spines['right'].set_color('r')
+        axs[1].yaxis.label.set_color('r')
+        axs[1].tick_params(axis='y', colors='r')
 
-        [ax.xaxis.set_minor_locator(AutoMinorLocator(2)) for ax in axs]
-        [ax.xaxis.set_major_formatter(mdates.DateFormatter("%d %H:%M")) for ax in axs]
+        grid_visual(axs[0])
+        ticks_visual(axs[0])
+        ticks_visual(axs[1])
 
-        # [ax.axes.xaxis.set_ticklabels([]) for ax in axs[:-1]]
-        [plt.setp(ax.get_xticklabels(), visible=False) for ax in axs[:-1]]
 
-        ax = axs[-1]
-        plt.sca(ax)  # set current axis
-        plt.xticks(rotation=25, ha="right")
-        ax = axs[0]
-        ax.text(
-            -0.05, 1.07, self.start.strftime("%Y, %d %b, %a, %H:%M"), transform=ax.transAxes,
-        )
+    def plot_current(self,**kws):
+        # plot current data
+        self.plot_data = kws.get("data", None)
+        if self.plot_data is None:
+            self.time_formatter(**kws)
+        pass
 
-        self.df_adc = df_adc
-        self.df_tc = df_tc
+    def plot_mfc(self,**kws):
+        # plot mfc data
+        # PresetP and sig are plotted in the same figure
+        self.plot_data = kws.get("data", None)
+        if self.plot_data is None:
+            self.time_formatter(**kws)
+        pass
 
-        # return fig
+    def show_calibration(self,**kws):
+        # show calibration data
+        self.plot_data = kws.get("data", None)
+        if self.plot_data is None:
+            self.time_formatter(**kws)
+        pass
+    
 
+    def time_formatter(self,**kws):
+        st = kws.get("st", self.start_time)
+        et = kws.get("et", self.end_time)
+
+        if type(st) == datetime:
+            st = int(st.strftime("%Y%m%d%H%M%S"))
+        if type(et) == datetime:
+            et = int(et.strftime("%Y%m%d%H%M%S"))
+
+        self.plot_data = self.data.query(f"{st} <= datetime <= {et}")
+
+        
