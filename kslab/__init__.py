47d89e8c97ad259af4f8ed45121f6f11534815d2
Kshora <119432291+Kshora@users.noreply.github.com>
2023-06-27 21:58:21 +0900

    Update __init__.py

--- kslab/__init__.py
+++ kslab/__init__.py
@@ -26,7 +26,7 @@ __all__ = ["graph_tools", "langmuirprobe", "qms", "spectrum", "raspi"]
 # limitations under the License.
 
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 """
     Release notes:
